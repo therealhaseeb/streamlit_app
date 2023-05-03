99f5415cdf97bc3f3462ebb38ba060111be1656f
Haseeb Zahid <zahidhaseeb46@gmail.com>
2023-05-03 21:44:41 +0500

    Update streamlit_app.py
    pandas dataframe integrate

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,11 @@ streamlit.header("Menu")
 streamlit.text("Khausey")
 streamlit.text("Kabab")
 streamlit.text("Biryani")
+
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.header("Build your own smoothie")
+streamlit.dataframe(my_fruit_list)
